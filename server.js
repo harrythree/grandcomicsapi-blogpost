2b95e60424c4044390a677a2e335c2863622a444
Harry Baldwin <harrythree@gmail.com>
2016-03-20 18:40:14 -0400

    Added next to error handling middleware in server.js

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(function(req, res, next) {
 	next(err);
 });
 
-app.use(function(err, req, res) {
+app.use(function(err, req, res, next) {
 	res.status(err.status || 500);
 	res.json({
 		message: err.message,

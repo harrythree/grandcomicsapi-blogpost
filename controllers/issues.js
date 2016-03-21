de50ed66b15922e7d65caeb1d15f68b6c0a580c8
Harry Baldwin <harrythree@gmail.com>
2016-03-20 22:37:42 -0400

    Issue filters

--- controllers/issues.js
+++ controllers/issues.js
@@ -3,6 +3,7 @@ var Issue = require('../models/Issue');
 module.exports = {
 	all: function(req, res, next) {
 		var seriesId = req.query.series_id;
+		var variantFilter = req.query.variants || 'none';
 
 		if (!seriesId) {
 			var err = new Error('The `series_id` parameter is required.');
@@ -14,6 +15,12 @@ module.exports = {
 		.query(function(qb) {
 			qb.limit(20);
 			qb.where({series_id: seriesId});
+			if (variantFilter === 'none' && variantFilter !== 'both') {
+				qb.whereNull('variant_of_id');
+			}
+			if (variantFilter === 'only' && variantFilter !== 'both') {
+				qb.whereNotNull('variant_of_id');
+			}
 		})
 		.fetchAll()
 		.then(function(results) {
